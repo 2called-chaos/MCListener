163eb89ca1c1ec54e134039d45d92afa5d592558
2called_chaos <admin@sp-mg.de>
2011-05-30 14:46:32 +0200

    Fixed message in getid function

--- mcl_files/commands/getid.php
+++ mcl_files/commands/getid.php
@@ -5,11 +5,14 @@ $aliases = 'id';
 function CMD_getid($MCL, $user, $params = array())
 {
   if(!count($params)) {
+    // no query
     $MCL->pm($user, 'You have to declare an alias!');
   } else {
     if(!array_key_exists($params[0], $MCL->system->itemmap)) {
+      // item was not found at all
       $MCL->pm($user, 'The alias >  ' . $params[0] . ' < was not found!');
     } else {
+      // item was found, print it to user
       $MCL->pm($user, 'The ID for ' . $params[0] . ' is ' . $MCL->system->itemmap[$params[0]]);
     }
   }
--- mcl_files/lib/mclistener.php
+++ mcl_files/lib/mclistener.php
@@ -7,7 +7,7 @@
 */
 class MCListener
 {
-  const VERSION = '0.1 (alpha build 272)';
+  const VERSION = '0.1 (alpha build 273)';
 
   public $config = null;
   public $sys = null;
