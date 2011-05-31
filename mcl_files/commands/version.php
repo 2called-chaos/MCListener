4b6d4d5bc7beebb20e085756ab1a55589d873853
2called_chaos <admin@sp-mg.de>
2011-06-01 00:35:11 +0200

    Fixed version command

--- mcl_files/commands/version.php
+++ mcl_files/commands/version.php
@@ -5,8 +5,8 @@ $aliases = '';
 function CMD_version($MCL, $user, $params = array())
 {
   if($MCL->isAdmin($user)) {
-    $this->pm($user, 'MCL Version: ' . VERSION);
-    $this->pm($user, 'Server Version: ' . !is_null($this->system->serverVersion) ? $this->system->serverVersion : 'unknown');
+    $MCL->pm($user, 'MCL Version: ' . MCListener::VERSION);
+    $MCL->pm($user, 'Server Version: ' . !is_null($MCL->system->serverVersion) ? $MCL->system->serverVersion : 'unknown');
   } else {
     $MCL->deny($user);
   }
