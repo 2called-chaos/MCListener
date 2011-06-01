17784688eb2854796a3036d5305a793493c848c3
2called-chaos <admin@sp-mg.de>
2011-06-01 22:54:46 +0200

    Dont show Minecraft server version if you can't grab it you fool (means script)!

--- mcl_files/commands/version.php
+++ mcl_files/commands/version.php
@@ -7,7 +7,7 @@ if(!function_exists('CMD_version')) {
   {
     if($MCL->isAdmin($user)) {
       $MCL->pm($user, 'MCL Version: ' . MCListener::VERSION);
-      $MCL->pm($user, 'Server Version: ' . !is_null($MCL->system->serverVersion) ? $MCL->system->serverVersion : 'unknown');
+      // $MCL->pm($user, 'Server Version: ' . !is_null($MCL->system->serverVersion) ? $MCL->system->serverVersion : 'unknown');
     } else {
       $MCL->deny($user);
     }
